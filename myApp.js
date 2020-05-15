6db33285b860020f68c37d01e09e94b1a8df3c15
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-15 15:36:29 +0900

    Add Get Handler For Name

--- myApp.js
+++ myApp.js
@@ -80,7 +80,12 @@ app.get("/:word/echo", function(req, res) {
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get("/name", function(req, res) {
+    const { first, last } = req.query;
+    res.json({
+        name: `${first} ${last}`
+    })
+});
   
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
